f17f0653c1c2e95f96b1c4fbd4550d1c39a39b1d
Jairo <jairopereira_dias@hotmail.com>
2020-05-18 03:38:29 -0300

    salvando os dados na table

--- curso-javascript-projeto-usuarios-master/controllers/UserController.js
+++ curso-javascript-projeto-usuarios-master/controllers/UserController.js
@@ -10,12 +10,28 @@ class UserController{
 
         this.formEl.addEventListener("submit", event => {
             event.preventDefault();
+
+        let btn = this.formEl.querySelector("[type=submit]");
+
+        btn.disabled = true;
+
         let values = this.getValues();
         
+        if(!values){
+            btn.disabled = false;
+            return false;
+        }
+
         this.getPhoto().then(
             (content) => {
+                
                 values.photo = content;
+                
                 this.addLine(values);
+
+                btn.disabled = false;
+
+                this.formEl.reset();
             
             },
             (e) => {
@@ -45,7 +61,12 @@ class UserController{
             fileReader.onerror = (e) => {
                 reject(e);
             }
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/avatar.png');
+            }
+            
         });
 
         
@@ -53,19 +74,35 @@ class UserController{
     getValues(){
       
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                
+                field.parentElement.classList.add('has-error');
+
+                isValid = false;
+
+            }
     
             if(field.name == "gender"){
                 if(field.checked){
                     user[field.name] = field.value;
                 }
                 
+            }else if(field.name == "admin"){
+                user[field.name] = field.checked;
+
             }else{
                 user[field.name] = field.value;
             }
         
         });
+        
+        if(!isValid){
+            return false;
+        }
         
         return new User(
             user.name,
@@ -82,18 +119,38 @@ class UserController{
 
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
+            <td>${(dataUser.admin) ? "Sim": "Não"}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>
-      `;
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>`;
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+            numberUsers++;
+            let user = JSON.parse(tr.dataset.user);
+            if(user._admin) numberAdmin++;
+            
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 }
